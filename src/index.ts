c4200b7c829c77af0254930e364e2ba689a042ab
yasg-uru <yashpawar12122004@gmail.com>
2024-09-04 00:39:07 +0530

    changed cors origin

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,8 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-wardrobe-frontend.vercel.app",
+    // origin: "http://localhost:5173",
     credentials: true,
   })
 );
