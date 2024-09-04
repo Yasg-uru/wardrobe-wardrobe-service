ed423e2654cc4d823def1af588be66826c12eb1d
yasg-uru <yashpawar12122004@gmail.com>
2024-09-04 15:49:38 +0530

    changed database of the wardrobe wardrobe service

--- src/util/connectDb.ts
+++ src/util/connectDb.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     const response = await mongoose.connect(
-      `mongodb+srv://yashpawar12122004:EuxmOAiMNH29yjjk@cluster0.9529p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      `mongodb+srv://<db_username>:Ji37o36pyuHYpsEN@wardrobe-cloth.u30pf.mongodb.net/?retryWrites=true&w=majority&appName=Wardrobe-cloth`
     );
     // const response = await mongoose.connect(process.env.MONGO_URI as string);
     console.log(`data base is connected : ${response.connection.host}`);
