aa4a8b64d3bbafd0008becff9b62cefeb2d596cd
yasg-uru <yashpawar12122004@gmail.com>
2024-09-05 13:01:00 +0530

    some small changes

--- src/util/connectDb.ts
+++ src/util/connectDb.ts
@@ -4,7 +4,7 @@ const connectDb = async () => {
   try {
     const response = await mongoose.connect(
      
-      "mongodb+srv://yashpawar12122004:UQhfsJxiUJUOVfbF@irctc-backend.z3tc7gu.mongodb.net/?retryWrites=true&w=majority&appName=IRCTC-backend"
+      "mongodb+srv://yashpawar12122004:OpPOgaLKAtU8JAYq@cloth-service.riiev.mongodb.net/?retryWrites=true&w=majority&appName=cloth-service"
     );
     // const response = await mongoose.connect(process.env.MONGO_URI as string);
     console.log(`data base is connected : ${response.connection.host}`);
