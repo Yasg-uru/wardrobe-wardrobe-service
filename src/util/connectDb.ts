67af357a76969882d7a139b15b5742aac2a9bbbe
yasg-uru <yashpawar12122004@gmail.com>
2024-09-05 12:30:06 +0530

    chandged db

--- src/util/connectDb.ts
+++ src/util/connectDb.ts
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     const response = await mongoose.connect(
-      process.env.MONGO_URI as string 
+     
+      "mongodb+srv://yashpawar12122004:UQhfsJxiUJUOVfbF@irctc-backend.z3tc7gu.mongodb.net/?retryWrites=true&w=majority&appName=IRCTC-backend"
     );
     // const response = await mongoose.connect(process.env.MONGO_URI as string);
     console.log(`data base is connected : ${response.connection.host}`);
